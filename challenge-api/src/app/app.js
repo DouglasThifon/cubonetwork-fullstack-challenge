e3d4fae53c0f0bf86e4996c70f3ab9841549091d
Jefferson Martins de Andrade <jeffersonmandrade@gmail.com>
2017-07-30 14:11:37 -0300

    Criando endpoint POST /employees

--- challenge-api/src/app/app.js
+++ challenge-api/src/app/app.js
@@ -1,22 +1,36 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import config from './config/config';
 import connect from './database';
 import Employee from './model/employee';
 
 const app = express();
-
+app.use(bodyParser.json());
 connect(config);
 
-app.get('/employees', (req, res) => {
-  Employee
-    .find({}, { __v: false, _id: false })
-    .then((employees) => {
-      res.json(employees);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
-});
+app.route('/employees')
+  .get((req, res) => {
+    Employee
+      .find({}, { __v: false, _id: false })
+      .then((employees) => {
+        res.json(employees);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  })
+  .post((req, res) => {
+    Employee
+      .create(req.body)
+      .then(() => {
+        res.sendStatus(201);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  },
+  );
 
 export default app;
---
+++ challenge-api/test/app-integration.js
@@ -0,0 +1,75 @@
+import supertest from 'supertest';
+import chai from 'chai';
+import app from '../src/app/app';
+import Employee from '../src/app/model/employee';
+
+const request = supertest(app);
+const expect = chai.expect;
+
+describe('Routes: /employees', () => {
+  afterEach((done) => {
+    Employee
+      .remove({})
+      .then(() => {
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  describe('GET /employees', () => {
+    before((done) => {
+      Employee
+        .create({
+          name: 'João',
+          lastname: 'Silva',
+          participation: 50.5,
+        })
+        .then(() => {
+          done();
+        })
+        .catch((err) => {
+          done(err);
+        });
+    });
+    it('should return a list with one employee', (done) => {
+      request
+        .get('/employees')
+        .end((err, res) => {
+          expect(res.body).to.be.eql([{
+            name: 'João',
+            lastname: 'Silva',
+            participation: 50.5,
+          }]);
+          done(err);
+        });
+    });
+  });
+
+  describe('POST /employees', () => {
+    it('should save a employee', (done) => {
+      request
+        .post('/employees')
+        .send({ name: 'Jefferson', lastname: 'Martins', participation: 15.3 })
+        .end((err, res) => {
+          if (err) {
+            done(err);
+          }
+          expect(res.status).to.be.eq(201);
+          Employee
+            .find({ name: 'Jefferson' })
+            .then((employee) => {
+              expect(employee).to.have.lengthOf(1);
+              expect(employee[0].name).to.be.eq('Jefferson');
+              expect(employee[0].lastname).to.be.eq('Martins');
+              expect(employee[0].participation).to.be.eq(15.3);
+              done();
+            })
+            .catch((dberr) => {
+              done(dberr);
+            });
+        });
+    });
+  });
+});
