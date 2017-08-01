932d4df4a0ad774fa55673af369cb4b105c76a9f
Jefferson Martins de Andrade <jeffersonmandrade@gmail.com>
2017-07-31 23:58:43 -0300

    Adicionando a dependência cors na API

--- challenge-api/src/app/app.js
+++ challenge-api/src/app/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import config from './config/config';
 import connect from './database';
 import Employee from './model/employee';
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 connect(config);
 
